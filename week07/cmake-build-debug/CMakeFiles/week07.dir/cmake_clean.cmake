6c2bb3a4bc628e29ca2fd7875d25b494cfe87c4f
fed0rus <anarsis007@gmail.com>
2020-10-01 11:18:35 +0300

    init folder

---
+++ week07/cmake-build-debug/CMakeFiles/week07.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/week07.dir/main.c.o"
+  "week07"
+  "week07.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/week07.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ week07/cmake-build-debug/CMakeFiles/week07.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for week07.
+# This may be replaced when dependencies are built.
